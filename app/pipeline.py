99a051fb08c753299e9426061133b3f6271f08ed
Nico Abrasado <nabr105@aucklanduni.ac.nz>
2026-02-19 11:00:16 +1300

    Expanded on the pipeline and finshed the requirements.txt

--- app/pipeline.py
+++ app/pipeline.py
@@ -10,6 +10,7 @@ from dotenv import load_dotenv
 import time
 
 def pinecone_setup(): 
+    # Sets up the pinecone index
     index_name = os.getenv("PINECONE_INDEX_NAME")
     pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
 
@@ -36,8 +37,25 @@ def clone_repo(url, path):
 
     # Error catching as there are some read-only files in the git files.
     try: 
+        print("Started cloning")
         repo = Repo.clone_from(url, path)
+        print("Finished cloning")
         return repo
     except Exception as e: 
         print("Failed to clone: {e}")
         raise 
+
+def process_files(path): 
+    # Function deals with parsing of valid files within the cloned repo
+    supported_extensions = (".py", ".js", ".ts", ".md")
+    file_paths = {}
+    
+    # Looping through the path, append valid file path with supported extensions
+    for root, dirs, files in os.walk(path): 
+        # Check if the files are a supported extension 
+        for filename in files: 
+            if filename.endswith(supported_extensions): 
+                # Appending the files
+                file_paths.append(os.path.join(root, filename))
+
+    return file_paths
